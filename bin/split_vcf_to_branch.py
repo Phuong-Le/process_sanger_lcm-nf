e90d2bfbd91454ffbaf7f2689c6f100808810081
Anh Phuong Le <al35@sanger.ac.uk>
2024-05-09 09:01:56 +0100

    BUG: fixed path for mutation matrix by branch

--- bin/split_vcf_to_branch.py
+++ bin/split_vcf_to_branch.py
@@ -16,7 +16,7 @@ def split_vcf(vcf_path, outdir, prefix):
     for branch in branches:
         vcf_branch = vcf[vcf['Branch'] == branch]
         outfile_with_header = f'{outdir}/vcf_with_header/{prefix}_{branch}_with_header.vcf'
-        outfile = f'{outdir}/vcf_no_header/{prefix}_{branch}.vcf'
+        outfile = f'{outdir}/matrix_generator/{prefix}_{branch}.vcf'
         vcf_branch.to_csv(outfile_with_header, index = False, sep = '\t')
         cmd = f"sed '1s/^/#/' {outfile_with_header} > {outfile}"
         subprocess.run(cmd, shell=True)
